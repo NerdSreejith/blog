c5f96b7e0857c019ddb242c1ec39683ccc142912
Sreejith C <sree@kali.kali>
2024-03-08 21:58:15 +0530

    template 60% complete

--- blogHome/models.py
+++ blogHome/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    Name = models.CharField(max_length=50,null=True)
+    title = models.CharField(max_length=100,null = True)
+    s_content = models.CharField(max_length=200,null=True)
+    content=models.TextField()
+    time = models.TimeField(auto_now_add=True) 
+    image =models.ImageField(upload_to="blog/images")
--- blogHome/views.py
+++ blogHome/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import Blog
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    blogs = Blog.objects.all()
+    return render(request,"index.html",{"blogs":blogs})
